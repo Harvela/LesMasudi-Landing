0030bfa411b8d2a984f8e558a8605363abe4480a
lemoissonM <murhulametre@gmail.com>
2024-05-26 16:05:33 +0300

    add gallery

--- src/templates/Galery.tsx
+++ src/templates/Galery.tsx
@@ -1,14 +1,29 @@
+'use client';
+
+import 'yet-another-react-lightbox/styles.css';
+import 'yet-another-react-lightbox/plugins/thumbnails.css';
+
+import { useState } from 'react';
+import PhotoAlbum from 'react-photo-album';
+import Lightbox from 'yet-another-react-lightbox';
+// import optional lightbox plugins
+import Fullscreen from 'yet-another-react-lightbox/plugins/fullscreen';
+import Slideshow from 'yet-another-react-lightbox/plugins/slideshow';
+import Thumbnails from 'yet-another-react-lightbox/plugins/thumbnails';
+import Zoom from 'yet-another-react-lightbox/plugins/zoom';
+
 import { galery } from '@/utils/galery';
 
 const Galery = () => {
+  const [selectedIndex, setSelectedIndex] = useState(-1);
   return (
     <div id="galery" className="relative z-20 px-8 py-5 lg:px-16">
       <div className="rounded-md">
         <h1 className="mb-2 text-lg font-bold text-blue lg:text-xl">
           GALLERIE
         </h1>
-        <div className="h-[4px] w-[4%] bg-blue" />
-        <div className="my-16 grid grid-cols-2 gap-8 text-blue md:grid-cols-3">
+        <div className="mb-5 h-[4px] w-[4%] bg-blue" />
+        {/* <div className="my-16 grid grid-cols-2 gap-8 text-blue md:grid-cols-3">
           {galery.map((g, index) => (
             <img
               key={index}
@@ -17,7 +32,30 @@ const Galery = () => {
               className="h-[300px] w-[500px] object-cover"
             />
           ))}
-        </div>
+        </div> */}
+        <PhotoAlbum
+          photos={galery.map((g) => ({
+            src: g.icon,
+            height: g.height,
+            width: g.width,
+          }))}
+          layout="rows"
+          targetRowHeight={150}
+          onClick={({ index }) => setSelectedIndex(index)}
+        />
+
+        <Lightbox
+          slides={galery.map((g) => ({
+            src: g.icon,
+            height: g.height,
+            width: g.width,
+          }))}
+          open={selectedIndex >= 0}
+          index={selectedIndex}
+          close={() => setSelectedIndex(-1)}
+          // enable optional lightbox plugins
+          plugins={[Fullscreen, Slideshow, Thumbnails, Zoom]}
+        />
       </div>
     </div>
   );
--- src/utils/galery.ts
+++ src/utils/galery.ts
@@ -1,20 +1,32 @@
 export const galery = [
   {
     icon: 'https://admin.harvely.com/uploads/Whats_App_Image_2024_04_16_at_11_00_27_283aaff535.jpg',
+    height: 900,
+    width: 1200,
   },
   {
     icon: 'https://admin.harvely.com/uploads/Whats_App_Image_2024_04_16_at_11_00_32_c82497bb23.jpg',
+    height: 1333,
+    width: 1000,
   },
   {
     icon: 'https://admin.harvely.com/uploads/Whats_App_Image_2024_04_16_at_11_00_29_2_37d2ea80a8.jpg',
+    height: 1333,
+    width: 1000,
   },
   {
     icon: 'https://admin.harvely.com/uploads/Whats_App_Image_2024_04_16_at_11_00_32_2_1_199f82620d.jpg',
+    height: 1333,
+    width: 1000,
   },
   {
     icon: 'https://admin.harvely.com/uploads/Whats_App_Image_2024_04_16_at_11_00_28_1_f9d2b6bc9c.jpg',
+    height: 1333,
+    width: 1000,
   },
   {
     icon: 'https://admin.harvely.com/uploads/Whats_App_Image_2024_04_16_at_11_00_31_1_8e52f4515c.jpg',
+    height: 1333,
+    width: 1000,
   },
 ];
